f0a578f3c412140aeb518b979962ca85527bb162
Isma <2023_Ismael.younes@iticbcn.cat>
2024-10-03 22:27:44 +0200

    exercici començat

---
+++ Article.java
@@ -0,0 +1,5 @@
+public class Article {
+    String nom;
+    double quantitat;
+    String unitats;
+}
---
+++ ControlEncarrecs.java
@@ -0,0 +1,12 @@
+public class ControlEncarrecs {
+   public static void main(String[] args) {
+    System.out.println("Quina operacio vols realitza:");
+    System.out.println();
+    System.out.println(" Generar un nou encàrrec(A)");
+    System.out.println();
+    System.out.println(" Mostrar un encàrrec (B)");
+    System.out.println();
+    System.out.println(" Mostrar un encàrrec(B)");
+   } 
+   public
+} 
