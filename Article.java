c76257a58c54475a7776a1e3623d660bf73d9c9e
Isma <2023_Ismael.younes@iticbcn.cat>
2024-10-09 17:26:53 +0200

    2 moduls finalitzat

--- Article.java
+++ Article.java
@@ -1,5 +1,31 @@
 public class Article {
-    String nom;
-    double quantitat;
-    String unitats;
+    private String nom;
+    private double quantitat;
+    private String unitats;
+
+    public Article(String nom, double quantitat, String unitats) {
+        this.nom = nom;
+        this.quantitat = quantitat;
+        this.unitats = unitats;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public double getQuantitat() {
+        return quantitat;
+    }
+    public void setQuantitat(double quantitat) {
+        this.quantitat = quantitat;
+    }
+    public String getUnitats() {
+        return unitats;
+    }
+    public void setUnitats(String unitats) {
+        this.unitats = unitats;
+    }
+    
 }
+
--- ControlEncarrecs.java
+++ ControlEncarrecs.java
@@ -1,12 +1,41 @@
 public class ControlEncarrecs {
    public static void main(String[] args) {
-    System.out.println("Quina operacio vols realitza:");
-    System.out.println();
-    System.out.println(" Generar un nou encàrrec(A)");
-    System.out.println();
-    System.out.println(" Mostrar un encàrrec (B)");
-    System.out.println();
-    System.out.println(" Mostrar un encàrrec(B)");
+      System.out.println("Quina operacio vols realitza:");
+      System.out.println();
+      System.out.println("-Generar un nou encàrrec(A)");
+      System.out.println();
+      System.out.println("-Mostrar un encàrrec (B)");
+      System.out.println();
+      System.out.println("-Sortir (C)");
+      System.out.println();
+      String resposta = menu(Entrada.readLine());
+      
+         
    } 
-   public
+   public static char menu(String resposta)  {
+      resposta= resposta.toUpperCase();
+      while (true) {
+         if (resposta.length()==1) {
+            if (resposta.charAt(0)== 'A') {
+               return 'A'
+            }else if (resposta.charAt(0)== 'B') {
+              return 'B'
+            }else if (resposta.charAt(0)== 'C') {
+               System.out.println("Fins aviat!");
+               return 'C' ;
+            }
+         }else{
+            System.out.println("Respon amb nomes una opció.");
+         }
+         System.out.println("Opció no disponible, torna a introduir la resposta");
+         resposta=(Entrada.readLine()).toUpperCase();
+      }
+       
+   }
+   public static void generaEncarrec(){
+      System.out.println();
+   }
+   //public static MostraEncarrec
+
+
 } 
